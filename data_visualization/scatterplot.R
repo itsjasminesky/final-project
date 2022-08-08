e749a7534675e41417b5480944eefcdbc4297330
moralice <moralic08@gmail.com>
2022-08-07 18:30:11 -0700

    new changes index

--- data_visualization/scatterplot.R
+++ data_visualization/scatterplot.R
@@ -8,7 +8,7 @@ happiness_df <- read.csv("2021.csv")
 View(happiness_df)
 
 happiness_country <- happiness_df %>%
-  select(Country.name)
+  select(Country)
 
 View(happiness_country)
 
@@ -17,16 +17,25 @@ data=head(happiness_df, 149)
 
 main = "Health vs GDP" 
 
-model <- lm(Healthy.life.expectancy ~ Logged.GDP.per.capita, data = happiness_df)
+model <- lm(Health..Life.Expectancy. ~ Economy..logged.GDP.per.Capita., data = happiness_df)
 summary(model)
 
 # 1/ add text with geom_text, use nudge to nudge the text
-ggplot(happiness_df, aes(x=Logged.GDP.per.capita, y=Healthy.life.expectancy)) +
+ggplot(happiness_df, aes(x=Economy..logged.GDP.per.Capita., y=Health..Life.Expectancy.)) +
   geom_point() + # Show dots
   geom_text(
-    label= data$Country.name,
+    label= data$Country,
     nudge_x = 0.25, nudge_y = 0.25, 
     check_overlap = T) + stat_smooth(method = "lm", se = FALSE)
 
 hist(model$residuals)
+
+
+world_happiness_21 <- read.csv('2021.csv')
+
+ggplot(world_happiness_21, aes(x = Region, y = Economy..logged.GDP.per.Capita., fill = Region)) +
+  geom_boxplot()+
+  labs(title=" GDP per capita by world region, 2021", x= "Country", y = "Economy (GDP/Capita)") +
+  theme(legend.position = "top")+
+  coord_flip()
 
